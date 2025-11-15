dbd57327fb34ac91e0da193c7be373c5c439ba35
Aditya-dhakad <dheerajdhakad2005@gmail.com>
2025-11-15 23:49:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById("resetBtn").addEventListener("click", function() {
+    location.reload();
+});
+
+function toggleMenu() {
+    document.getElementById("mobileMenu").classList.toggle("open");
+}
+    
